cdd790981e7b5945f223bc4fc6a9686ed6b4f88c
Matthew Kumar <mattkumar@gmail.com>
2018-07-22 13:20:07 +0000

    version 0.1.0

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+#' @import utils
+utils::globalVariables(c("ratio","difference","se","reference","s_rate","lower","upper"))
+utils::globalVariables(c("d", "pop", "wts", "cr_rate", "cr_var","st_rate","st_var","c_rate", "c_lower","c_upper","s_rate","s_lower","s_upper"))
